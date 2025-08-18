5b982a43afe86170706e5835bfa457734ae89880
Uri Kiro <yu.shapkarin@gmail.com>
2025-08-19 02:49:26 +0300

    fix: remove extra parameter in wallet creation call

--- app/routes/import-wallet.tsx
+++ app/routes/import-wallet.tsx
@@ -80,7 +80,6 @@ export default function ImportWallet() {
         seedPhraseData.hash,
         walletName.trim(),
         chainId,
-        derivationPath
       );
 
       dispatch(addSeedPhrase(seedPhraseData));
